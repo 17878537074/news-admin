184461cc5c5f774f2684129316137c39e88175ad
马垚谟 <1085482669@qq.com>
2020-04-12 22:21:31 +0800

    黑马头条管理后台

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+// Element组件完整导入
+import ElementUI from 'element-ui';
+// Element组件样式
+import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   router,
   render: h => h(App)
